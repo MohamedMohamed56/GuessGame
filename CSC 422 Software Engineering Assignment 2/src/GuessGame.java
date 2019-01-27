3df844801424dc76dabec1ca35faf7cdfcd4d548
Mohamed Mohamed <mohamedm14@csp.edu>
2019-01-27 13:19:29 -0600

    Empty Java File to Repository for GuessingGame Project.

---
+++ CSC 422 Software Engineering Assignment 2/src/GuessGame.java
@@ -0,0 +1,24 @@
+/*
+ 
+ Assignment Name: Assignment 2
+
+ Instructor: Dr.Cheng Thao. 
+
+ Written By: Mohamed Mohamed.
+
+ Course: CSC 422- Software Engineering Class
+
+ Written: 1/26/2019.
+
+ Revised: 1/27/2019.
+ 
+*/
+
+public class GuessGame { // This is our GuessGame class.
+
+	public static void main(String[] args) {// main method for our java program.
+		// TODO Auto-generated method stub
+
+	}
+
+}
