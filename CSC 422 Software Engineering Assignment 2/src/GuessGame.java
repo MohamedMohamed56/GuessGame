057f0ffdf90884ac6a12cfe9b3595dd595561fc4
Mohamed Mohamed <mohamedm14@csp.edu>
2019-01-29 17:08:32 -0600

    Third Version

--- CSC 422 Software Engineering Assignment 2/src/GuessGame.java
+++ CSC 422 Software Engineering Assignment 2/src/GuessGame.java
@@ -1,35 +1,46 @@
+
+import java.util.Scanner;
+
 /*
- 
- Assignment Name: Assignment 2
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
- Instructor: Dr.Cheng Thao. 
+/**
+ *
+ * @author Mohamed Mohamed
+ */
 
- Written By: Mohamed Mohamed.
+/*
 
- Course: CSC 422- Software Engineering Class
+Assignment Name: Assignment 2
 
- Written: 1/26/2019.
+Instructor: Dr.Cheng Thao. 
 
- Revised: 1/27/2019.
- 
- Version #2
- 
+Written By: Mohamed Mohamed.
+
+Course: CSC 422- Software Engineering Class
+
+Written: 1/26/2019.
+
+Revised: 1/27/2019.
+
+Version #3
+
 */
 
-import java.util.Scanner; // Importing our Java Scanner class. 
-
-public class GuessGame { // This is our GuessGame class.
-
-public static void main(String[] args) {// Main method for our java program.
-
-        
+public class GuessGame { // This is our GuessGame class
+   
+   public static void main(String[] args) {// Main method for our java program.
+       
 	Scanner input = new Scanner(System.in); //Reads from keyboard.
 	
-	int number = (int) (Math.random() * 99 + 1); //  Declaring the number and using it with math.random class
+       int number = (int) (Math.random() * 99 + 1); //  Declaring the number and using it with math.random class
 		
-	int guess; // declaring guess variable as int
-   	
-        // using a do while loop to iterate the multiple guess the uses inputs.
+       int guess; // declaring guess variable as int
+       
+      // Using a do while loop to iterate the multiple guess the uses inputs.
 	do {
 		System.out.print("I'm thinking of a number between 0-100. "); // Displays the user's question about  thinking a number between 0-20.
 
@@ -37,17 +48,22 @@ public static void main(String[] args) {// Main method for our java program.
 
 		guess = input.nextInt();
 				
-		if(guess == number)  // if guess is correct the the user gets it correct
+		if(guess == number)  // if guess is correct the the user gets it correct.
 			
-			System.out.println("You got it! " + guess);
+	             System.out.println("You Got it!");
 			
-		else  // if the guess is incorrect than the user needs to try  again.
-			System.out.println("Nope. " + "Try Again: " + guess);
+		else if (guess < number) // if the guess is less than the number then the it will be too low and would have to try again.
+			
+                    System.out.println("Nope. " + "Too Low. " + "Try Again: " + guess);
 
-	} while (guess != number);
-       
-        // End of do-while loop.
-}
-
+         else if (guess > number) // if the guess is greater then it will be too high and the user will have to try again.
+			
+                    System.out.println("Nope. " + "Too High. "+ "Try Again: " + guess);
+	
+       } while (guess != number);
     
+     // End of do while loop.
 }
+
+}
+        
