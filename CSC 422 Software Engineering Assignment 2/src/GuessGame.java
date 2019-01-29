ac07b17541325b3bf0909e3c8c4d78fe5d6614f6
Mohamed Mohamed <mohamedm14@csp.edu>
2019-01-29 12:57:57 -0600

    Second Version

--- CSC 422 Software Engineering Assignment 2/src/GuessGame.java
+++ CSC 422 Software Engineering Assignment 2/src/GuessGame.java
@@ -12,7 +12,7 @@
 
  Revised: 1/27/2019.
  
- Version #1
+ Version #2
  
 */
 
@@ -20,39 +20,34 @@ import java.util.Scanner; // Importing our Java Scanner class.
 
 public class GuessGame { // This is our GuessGame class.
 
-	public static void main(String[] args) {// Main method for our java program.
-	 
+public static void main(String[] args) {// Main method for our java program.
+
+        
 	Scanner input = new Scanner(System.in); //Reads from keyboard.
 	
-	int number = 1 + (int) (Math.random() * 20); // Using  Math.Random to set the randomNumber
-	
-	int guess = 0; // Guess is set to 0.
-	
-	int count = 0; //Count is set to 0.
+	int number = (int) (Math.random() * 99 + 1); //  Declaring the number and using it with math.random class
 		
-    System.out.print("I'm thinking of a number between 0-20. "); // Displays the user's question about  thinking a number between 0-20.
-	   
-	System.out.print("What is the number? "); //  Display to the user about which number they have inputted for our guessing game.
-	
-	// Using while loop to iterate through the guess of the user
-	
-	while(guess != number) {
-		 
-		 guess = input.nextInt();
-		 
-		 count++;
-		 
-		 if(guess > number) { // checking if guess is greater than randomNumber
-			 
-		   System.out.print("Nope " + "The number is " + number); // Displays the guess as wrong if the user inputs the wrong guess.
-			 
-		 }
-		 
-		 else if(guess < number ) { // checking if guess is less than randomNumber.
-			  
-			 System.out.print("Yes"); // Displays the guest a correct if the user inputs the correct guess.
-		 }
-	 }
-	
-	}
+	int guess; // declaring guess variable as int
+   	
+        // using a do while loop to iterate the multiple guess the uses inputs.
+	do {
+		System.out.print("I'm thinking of a number between 0-100. "); // Displays the user's question about  thinking a number between 0-20.
+
+		System.out.print("What is the number? "); //  Display to the user about which number they have inputted for our guessing game.
+
+		guess = input.nextInt();
+				
+		if(guess == number)  // if guess is correct the the user gets it correct
+			
+			System.out.println("You got it! " + guess);
+			
+		else  // if the guess is incorrect than the user needs to try  again.
+			System.out.println("Nope. " + "Try Again: " + guess);
+
+	} while (guess != number);
+       
+        // End of do-while loop.
+}
+
+    
 }
